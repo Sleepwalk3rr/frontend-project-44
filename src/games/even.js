94e3748437d1bcf5f7920c87260878004bb58fe6
Sleepwalk3r <sad.toy.channel@gmail.com>
2026-06-09 23:38:38 +0300

    game even

---
+++ src/games/even.js
@@ -0,0 +1,35 @@
+import readlineSync from 'readline-sync';
+
+const runEvenGame = () => {
+  console.log('Welcome to the Brain Games!');
+
+  const name = readlineSync.question('May I have your name? ');
+
+  console.log(`Hello, ${name}!`);
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+  let correctAnswers = 0;
+
+  while (correctAnswers < 3) {
+    const number = Math.floor(Math.random() * 100);
+
+    console.log(`Question: ${number}`);
+
+    const answer = readlineSync.question('Your answer: ');
+
+    const correctAnswer = number % 2 === 0 ? 'yes' : 'no';
+
+    if (answer === correctAnswer) {
+      console.log('Correct!');
+      correctAnswers += 1;
+    } else {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${name}!`);
+      return;
+    }
+  }
+
+  console.log(`Congratulations, ${name}!`);
+};
+
+export default runEvenGame;
